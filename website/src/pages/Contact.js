720e94211c3592c7131a13d5b079c8b3670dd37b
dwekwe <151397396+dwekwe@users.noreply.github.com>
2023-11-19 03:27:58 -0700

    Add files via upload

---
+++ website/src/pages/Contact.js
@@ -0,0 +1,68 @@
+function Contact() {
+    return (
+        <section id="contact">
+            <div class="contact container">
+                <div><h1 class="section-title">Contact <span>info</span></h1></div>
+                <div class="contact-items">
+                    <div class="contact-item">
+                        <div class="icon"><img src="https://img.icons8.com/bubbles/100/0000000/phone.png"/></div>
+                        <div class="contact-info">
+                            <h1>Name & Github</h1>
+                            <h2>HackEDBetaTeam2023</h2>                            
+                            
+                            <a href="https://github.com/HackEDBetaTeam2023/CPR_Tutorial_HackED_Beta">HackedBeta Github</a>
+                            
+                            <h2>GYURIM DO</h2>
+                            <h2>Ben</h2>
+                            <a href="https://github.com/Ben3050">Github</a>
+                            <h2>Crystal Cho</h2>
+                            <a href="https://github.com/crystal11111">Github</a>
+                            <h2>Rorgon</h2>
+                            <a href="https://github.com/RordyC">Github</a>
+                            <h2>Daniel</h2>
+                            <a href="https://github.com/ChappydevC">Github</a>
+                          
+
+                        </div>
+
+
+                    </div>
+                    <div class="contact-item">
+                        <div class="icon"><img src="https://img.icons8.com/bubbles/100/0000000/new-post.png"/></div>
+                        <div class="contact-info">
+                            <h1>Email</h1>
+                            <h2>Gyurim: gyurim@ualebrta.ca</h2>
+                            <h2>Ben: bshem@ualberta.ca </h2>
+                            <h2>Crystal: rystal031230@gmail.com</h2>
+                            <h2>Rorgon: rordyc@gmail.com</h2>
+                            <h2>Daniel: danjtchapman@gmail.com</h2>
+                        </div>
+
+
+                    </div>
+                    <div class="contact-item">
+                        <div class="icon"><img src="https://img.icons8.com/bubbles/100/0000000/map-marker.png"/></div>
+                        <div class="contact-info">
+                            <h1>linkedin</h1>
+                            <h2>Crystal</h2>
+                            <a href="www.linkedin.com/in/soojeongcho1230">Linkedin</a>
+                            <h2>Daniel</h2>
+                            <a href="www.linkedin.com/in/danjtchapman">Linkedin</a>
+                            
+                    
+                        </div>
+
+
+                    </div>
+                </div>
+                
+            </div>
+
+
+
+        </section>
+        
+        
+    )
+}
+export default Contact;
