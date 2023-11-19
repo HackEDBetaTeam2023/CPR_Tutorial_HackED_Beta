6c4f53988672eb2b0175b497e74077259bd34e6a
ChappyDev <danjtchapman@gmail.com>
2023-11-18 22:52:44 -0700

    Update layout of NavBar

--- website/src/components/NavBar.js
+++ website/src/components/NavBar.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import './NavBar.css';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <header>
-      <div id="navbar">
-        <span id="navbar-name">Name</span>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/service">Service</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-          </ul>
-        </nav>
-      </div>
+      <nav id="navbar">
+        <h1 id="navbar-name">Name</h1>
+        <ul className="link-list">
+            <li><Link className="navbar-link" to="/">Home</Link></li>
+            <li><Link className="navbar-link" to="/service">Service</Link></li>
+            <li><Link className="navbar-link" to="/about">About</Link></li>
+            <li><Link className="navbar-link" to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
     </header>
   );
 };
